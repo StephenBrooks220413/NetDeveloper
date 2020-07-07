0d117da0b3466dd90ca49e2fa67f22e205b8d988
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-07-06 18:27:22 -0700

    Add project files.

---
+++ NetDeveloper/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace NetDeveloper.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ NetDeveloper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetDeveloper.Global" Language="C#" %>
